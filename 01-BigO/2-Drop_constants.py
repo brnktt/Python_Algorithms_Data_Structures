d8bfbf5339c9f5e117dcd4cb018f0b0f170addb2
Alessandro Branchetti <brk.alessandro@gmail.com>
2023-05-29 15:47:25 +0200

    Drop constants

---
+++ 01-BigO/2-Drop_constants.py
@@ -0,0 +1,8 @@
+def print_items(n): #O(n + n = 2n) -> O(n)
+    for i in range(n): #O(n) -> linear(proportional)
+        print(i)
+        
+    for j in range(n): #O(n) -> linear(proportional)
+        print(j)
+        
+print_items(10)
