a54a64387c85d023277abc8d9b1227c4102401bd
Alessandro Branchetti <brk.alessandro@gmail.com>
2023-05-29 16:02:41 +0200

    O(n^2)

---
+++ 01-BigO/3-O(n^2).py
@@ -0,0 +1,6 @@
+def print_items(n): #O(n * n = n^2) -> O(n^2)
+    for i in range(n): #O(n) -> linear(proportional)
+        for j in range(n): #O(n) -> linear(proportional)
+            print(i, j)
+        
+print_items(10)
